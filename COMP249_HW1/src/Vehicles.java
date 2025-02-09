d5e087401dc8d6925754c84490002b943a9bca77
Jacob Paterak <jacobpaterak@gmail.com>
2025-02-09 17:40:43 -0500

    Commit 5

--- COMP249_HW1/src/Vehicles.java
+++ COMP249_HW1/src/Vehicles.java
@@ -1,4 +1,53 @@
 
-public class Vehicles {
+public class Vehicles 
+{
+private String Plate_Number;
+private String Make;
+private String Model;
+private int Year_of_Production;
+
+public Vehicles()
+{
+	
+}
+public Vehicles(String PN, String Make, String Model, int YOP)
+{
+	
+}
+public String getPlate_Number()
+{
+	return this.Plate_Number;
+}
+public String getMake()
+{
+	return this.Make;
+}
+
+public String getModel()
+{
+	return this.Model;
+}
+
+public int getYear_of_Production()
+{
+	return this.Year_of_Production;
+}
+public void setPlate_Number(String Plate_Num)
+{
+	
+}
+public void setMake(String Make) 
+{
+	
+}
+public void setModel(String Model)
+{
+	
+}
+public void setYear_Of_Production(int Year_of_Production)
+{
+	
+}
+
 
 }
