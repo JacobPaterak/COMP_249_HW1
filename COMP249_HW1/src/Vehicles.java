f100d9fb12de3c0e26bcd9f6ebf6f189e77fe0ab
Augustin Redon <augustin.redon@icloud.com>
2025-02-19 15:59:27 -0500

    Indentation

--- COMP249_HW1/src/Vehicles.java
+++ COMP249_HW1/src/Vehicles.java
@@ -1,65 +1,60 @@
 
-public class Vehicles 
-{
-public String Plate_Number;
-public String Make;
-public String Model;
-public int Year_of_Production;
+public class Vehicles {
+	public String Plate_Number;
+	public String Make;
+	public String Model;
+	public int Year_of_Production;
 
-public Vehicles()
-{
+	public Vehicles(){ }
 	
-}
-public Vehicles(String Make, String Model, int YOP)
-{
-	this.Make = Make;
-	this.Model = Model;
-	this.Year_of_Production = YOP;
-}
-public String getPlate_Number()
-{
-	return this.Plate_Number;
-}
-public String getMake()
-{
-	return this.Make;
-}
+	public Vehicles(String Make, String Model, int YOP){
+		this.Make = Make;
+		this.Model = Model;
+		this.Year_of_Production = YOP;
+	}
 
-public String getModel()
-{
-	return this.Model;
-}
+	public String getPlate_Number(){
+		return this.Plate_Number;
+	}
 
-public int getYear_of_Production()
-{
-	return this.Year_of_Production;
-}
-public void setPlate_Number(String Plate_Num)
-{
-	this.Plate_Number = Plate_Num;
-}
-public void setMake(String Make) 
-{
-this.Make = Make;	
-}
-public void setModel(String Model)
-{
-	this.Model = Model;
-}
-public void setYear_Of_Production(int Year_of_Production)
-{
-	this.Year_of_Production = Year_of_Production;
-}
-public void Copy(Vehicles obj)
-{
-	this.Make = obj.getMake();
-	this.Model = obj.getModel();
-	this.Year_of_Production = obj.getYear_of_Production();
-}
-public String toString()
-{
-	return "The model is " + this.Model + " the make is " +  this.Make + " the year of production is " + this.Year_of_Production;
-}
+	public String getMake(){
+		return this.Make;
+	}
+
+	public String getModel(){
+		return this.Model;
+	}
+
+	public int getYear_of_Production(){ 
+		return this.Year_of_Production;
+	}
+
+	public void setPlate_Number(String Plate_Num){ 
+		this.Plate_Number = Plate_Num; 
+	}
+	
+	public void setMake(String Make) { 
+		this.Make = Make; 
+	}
+	
+	public void setModel(String Model) { 
+		this.Model = Model; 
+	}
+	
+	public void setYear_Of_Production(int Year_of_Production){
+		this.Year_of_Production = Year_of_Production;
+	}
+
+	public void Copy(Vehicles obj){
+		this.Make = obj.getMake();
+		this.Model = obj.getModel();
+		this.Year_of_Production = obj.getYear_of_Production();
+	}
+
+
+	public String toString(){
+		return "The model is " + this.Model + " the make is " +  this.Make + " the year of production is " + this.Year_of_Production;
+	}
 
 
 }
