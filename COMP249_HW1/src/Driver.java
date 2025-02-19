b2f7388e89ddfcbd80d5229bdf0284377bccaabc
Augustin Redon <augustin.redon@icloud.com>
2025-02-19 16:50:56 -0500

    worked on truck classes + some other tests

--- COMP249_HW1/src/Driver.java
+++ COMP249_HW1/src/Driver.java
@@ -1,10 +1,10 @@
 
 public class Driver {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args){
 		Vehicles v1 = new Vehicles();
 		Cars c1 = new Cars();
+		Diesel_Truck myDiesel_Truck = new Diesel_Truck("Ram", "1", 102, 1, 1);
 		System.out.println(c1);
 		System.out.println(v1);
 	}
--- COMP249_HW1/src/Truck.java
+++ COMP249_HW1/src/Truck.java
@@ -1,4 +1,16 @@
 
-public class Truck {
+
+public class Truck extends Vehicles{
+
+    private double max_weight_capacity;
+
+    public Truck(){
+        super();
+    }
+
+    public Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity){
+        super(Make, Model, Year_of_Production);
+        this.max_weight_capacity = max_weight_capacity;
+    }
 
 }
