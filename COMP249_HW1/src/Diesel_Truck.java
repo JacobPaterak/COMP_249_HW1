16e89d46c0488b4ba6c78389995a166dd1652f7d
Augustin Redon <augustin.redon@icloud.com>
2025-02-20 11:18:45 -0500

    Finalized class & added Comments

--- COMP249_HW1/src/Diesel_Truck.java
+++ COMP249_HW1/src/Diesel_Truck.java
@@ -2,37 +2,52 @@
 public class Diesel_Truck extends Truck{
     
     private static int dieselTruckCounter = 1000;
-    private double fuel_capacity;
-    private String Plate_Number;
+    private double fuelCapacity;
+    private String plateNumber;
 
+    //Default Constructor
     public Diesel_Truck(){
         super();
+        this.fuelCapacity = 0.0;
+        this.plateNumber = "DT" + dieselTruckCounter++;
     }
 
-    public Diesel_Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity, double fuel_capacity){
+    //Parameterized Constructor
+    public Diesel_Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity, double fuelCapacity){
         super(Make, Model, Year_of_Production, max_weight_capacity);
-        this.fuel_capacity = fuel_capacity;
-        this.Plate_Number = "DT" + dieselTruckCounter++;
+        this.fuelCapacity = fuelCapacity;
+        this.plateNumber = "DT" + dieselTruckCounter++;
     }
 
-    public void setFuel_capacity(double fuel_capacity){
-        this.fuel_capacity  = fuel_capacity;
+    //Copy Constructor
+    public Diesel_Truck(Diesel_Truck obj){
+        super(obj);
+        this.fuelCapacity = obj.getfuelCapacity();
+        this.plateNumber = "DT" + dieselTruckCounter++;
     }
 
-    public double getFuel_capacity(){
-        return fuel_capacity;
+    //Setter
+    public void setfuelCapacity(double fuelCapacity){
+        this.fuelCapacity  = fuelCapacity;
     }
 
+    //Getter
+    public double getfuelCapacity(){
+        return fuelCapacity;
+    }
+
+    @Override
     public String toString(){
-        return super.toString() + this.fuel_capacity;
+        return super.toString() + ", Fuel Tank Capacity: " + this.fuelCapacity + "L, Plate Number: " + plateNumber;
     }
 
+    @Override
     public boolean equals(Object obj){
 		if(obj == null || !(obj instanceof Diesel_Truck)){
             return false;
         }
 		Diesel_Truck obj2 = (Diesel_Truck) obj;
-		return this.getMake().equals(obj2.getMake()) && this.getModel().equals(obj2.getModel()) && this.getYear_of_Production() == obj2.getYear_of_Production() && this.getMax_weight_capacity() == obj2.getMax_weight_capacity() && this.getFuel_capacity() == obj2.getFuel_capacity();
+		return this.getMake().equals(obj2.getMake()) && this.getModel().equals(obj2.getModel()) && this.getYear_of_Production() == obj2.getYear_of_Production() && this.getMax_weight_capacity() == obj2.getMax_weight_capacity() && this.getfuelCapacity() == obj2.getfuelCapacity();
 		
 	}
 
