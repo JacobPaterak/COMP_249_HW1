ca639d861c7d97ae6adb12ffeb9199a4ee122a50
Augustin Redon <augustin.redon@icloud.com>
2025-02-19 23:04:36 -0500

    Set up all but copy class

--- COMP249_HW1/src/Diesel_Truck.java
+++ COMP249_HW1/src/Diesel_Truck.java
@@ -15,7 +15,26 @@ public class Diesel_Truck extends Truck{
         this.Plate_Number = "DT" + dieselTruckCounter++;
     }
 
+    public void setFuel_capacity(double fuel_capacity){
+        this.fuel_capacity  = fuel_capacity;
+    }
 
+    public double getFuel_capacity(){
+        return fuel_capacity;
+    }
+
+    public String toString(){
+        return super.toString() + this.fuel_capacity;
+    }
+
+    public boolean equals(Object obj){
+		if(obj == null || !(obj instanceof Diesel_Truck)){
+            return false;
+        }
+		Diesel_Truck obj2 = (Diesel_Truck) obj;
+		return this.getMake().equals(obj2.getMake()) && this.getModel().equals(obj2.getModel()) && this.getYear_of_Production() == obj2.getYear_of_Production() && this.getMax_weight_capacity() == obj2.getMax_weight_capacity() && this.getFuel_capacity() == obj2.getFuel_capacity();
+		
+	}
 
     
 
