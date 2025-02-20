fa4ee4635173e17cfddcabe4db829714aba0da3b
AugustinREDON <augustin.redon@gmail.com>
2025-02-19 22:04:34 -0500

    Update Diesel_Truck.java

--- COMP249_HW1/src/Diesel_Truck.java
+++ COMP249_HW1/src/Diesel_Truck.java
@@ -1,4 +1,22 @@
 
-public class Diesel_Truck {
+public class Diesel_Truck extends Truck{
+    
+    private static int dieselTruckCounter = 1000;
+    private double fuel_capacity;
+    private String Plate_Number;
+
+    public Diesel_Truck(){
+        super();
+    }
+
+    public Diesel_Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity, double fuel_capacity){
+        super(Make, Model, Year_of_Production, max_weight_capacity);
+        this.fuel_capacity = fuel_capacity;
+        this.Plate_Number = "DT" + dieselTruckCounter++;
+    }
+
+
+
+    
 
 }
