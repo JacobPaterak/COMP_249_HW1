2d5f8523f8fbdb231a8051fc62ba106419f63ac0
Augustin Redon <augustin.redon@icloud.com>
2025-02-20 10:30:57 -0500

    Fixed allignment

--- COMP249_HW1/src/Gasoline_Car.java
+++ COMP249_HW1/src/Gasoline_Car.java
@@ -3,31 +3,30 @@ public class Gasoline_Car extends Cars
 {
 	private int plate_number;
 	private static int counter = 1;
-	public Gasoline_Car()
-	{
+	public Gasoline_Car(){
 		super();
 	}
-	public Gasoline_Car(String Make, String Model,int YOP, int Max_People)
-	{
+
+	public Gasoline_Car(String Make, String Model,int YOP, int Max_People){
 		super(Make,Model,YOP,Max_People);
 	}
-	public String toString()
-	{
+
+	public String toString(){
 		return super.toString();
 	}
+
 //	public void setPlate_Num()
 //	{
 //		
 //	}
-	public String getPlate_Num()
-	{
+
+	public String getPlate_Num(){
 		this.plate_number = 1000 + counter;
 		counter++;
 		return "GC" + this.plate_number;
 	}
 	
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj){
 		if(obj == null || !(obj instanceof Gasoline_Car))
 			return false;
 		Gasoline_Car obj2 = (Gasoline_Car)obj;
