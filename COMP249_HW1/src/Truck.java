8d89e7bf85ce6e979f99b1458711b646c8d4e846
Augustin Redon <augustin.redon@icloud.com>
2025-02-19 23:05:26 -0500

    Set up all but copy method

--- COMP249_HW1/src/Truck.java
+++ COMP249_HW1/src/Truck.java
@@ -11,4 +11,24 @@ public class Truck extends Vehicles{
         this.max_weight_capacity = max_weight_capacity;
     }
 
+    public void setMax_weight_capacity(double max_weight_capacity){
+        this.max_weight_capacity = max_weight_capacity;
+    }
+
+    public double getMax_weight_capacity(){
+        return max_weight_capacity;
+    }
+
+    public String toString(){
+        return super.toString() + this.max_weight_capacity;
+    }
+
+    public boolean equals(Object obj){
+        if(obj == null || !(obj instanceof Truck)){
+            return false;
+        }
+        Truck obj2 = (Truck) obj;
+        return this.getMake().equals(obj2.getMake()) && this.getModel().equals(obj2.getModel()) && this.getYear_of_Production() == obj2.getYear_of_Production() && this.getMax_weight_capacity() == obj2.getMax_weight_capacity();
+    }
+
 }
