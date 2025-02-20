ef5b4ba12aae8ee0cbae6d856bac8d412a92608d
Augustin Redon <augustin.redon@icloud.com>
2025-02-20 12:58:05 -0500

    packages

--- COMP249_HW1/src/Clients.java
+++
@@ -1,8 +0,0 @@
-
-public class Clients
-{
-//Create client objeccts nnand store vehicles arrays inside
-//Ex Client josh = new CLient()
-//this.vehicles[] and we can put the adddition and subtraction of array size in here too
-//
-}
--- COMP249_HW1/src/Diesel_Truck.java
+++ COMP249_HW1/src/Diesel_Truck.java
@@ -1,3 +1,4 @@
+package Vehicle;
 
 public class Diesel_Truck extends Truck{
     
