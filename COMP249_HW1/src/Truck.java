d8567c73653181d5da0f5c712a7b2d0fc0055b75
AugustinREDON <augustin.redon@gmail.com>
2025-02-19 22:03:33 -0500

    Update Truck.java

--- COMP249_HW1/src/Truck.java
+++ COMP249_HW1/src/Truck.java
@@ -1,4 +1,14 @@
+public class Truck extends Vehicles{
 
-public class Truck {
+    private double max_weight_capacity;
+
+    public Truck(){
+        super();
+    }
+
+    public Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity){
+        super(Make, Model, Year_of_Production);
+        this.max_weight_capacity = max_weight_capacity;
+    }
 
 }
