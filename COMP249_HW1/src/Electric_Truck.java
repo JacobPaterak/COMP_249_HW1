9d198e23d64067cc7490008736ba9be43f31301b
Augustin Redon <augustin.redon@icloud.com>
2025-02-20 11:06:14 -0500

    Finalized Class & added comments

--- COMP249_HW1/src/Electric_Truck.java
+++ COMP249_HW1/src/Electric_Truck.java
@@ -1,38 +1,53 @@
 
 public class Electric_Truck extends Truck{
 
-    private double max_autonomy_range;
+    private double maxAutonomyRange;
     private static int electricTruckCounter = 1000;
     private String plateNumber;
 
+    //Default Constructor
     public Electric_Truck(){
         super();
     }
 
-    public Electric_Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity, double max_autonomy_range){
+    //Parameterized Constructor
+    public Electric_Truck(String Make, String Model, int Year_of_Production, double max_weight_capacity, double maxAutonomyRange){
         super(Make, Model, Year_of_Production, max_weight_capacity);
-        this.max_autonomy_range = max_autonomy_range;
+        this.maxAutonomyRange = maxAutonomyRange;
         this.plateNumber= "EC" + electricTruckCounter++;
     }
 
-    public void setMax_autonomy_range(double max_autonomy_range){
-        this.max_autonomy_range = max_autonomy_range;
+    //Copy Constructor
+    public Electric_Truck(Electric_Truck obj){
+        super(obj);
+        this.maxAutonomyRange = obj.getMaxAutonomyRange();
+        this.plateNumber = "EC" + electricTruckCounter++;
+
     }
 
-    public double getMax_autonomy_range(){
-        return max_autonomy_range;
+    //Setter
+    public void setMaxAutonomyRange(double maxAutonomyRange){
+        this.maxAutonomyRange = maxAutonomyRange;
     }
 
+    //Getter
+    public double getMaxAutonomyRange(){
+        return maxAutonomyRange;
+    }
+
+
+    @Override
     public String toString(){
-        return super.toString() + this.max_autonomy_range;
+        return super.toString() + ", Max Autonomy Range: " + this.maxAutonomyRange + "km, Plate Number: " + plateNumber;
     }
 
+    @Override
     public boolean equals(Object obj){
         if(obj == null || !(obj instanceof Electric_Truck)){
             return false;
         }
         Electric_Truck obj2 = (Electric_Truck) obj;
-        return this.getMake().equals(obj2.getMake()) && this.getModel().equals(obj2.getModel()) && this.getYear_of_Production() == obj2.getYear_of_Production() && this.getMax_weight_capacity() == obj2.getMax_weight_capacity() && this.getMax_autonomy_range() == obj2.getMax_autonomy_range();
+        return this.getMake().equals(obj2.getMake()) && this.getModel().equals(obj2.getModel()) && this.getYear_of_Production() == obj2.getYear_of_Production() && this.getMax_weight_capacity() == obj2.getMax_weight_capacity() && this.getMaxAutonomyRange() == obj2.getMaxAutonomyRange();
 
     }
 
